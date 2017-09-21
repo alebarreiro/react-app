a12240e72b895f22ccaf9718978edddbf7bcc492
Alejandro Barreiro <alebarreiro91@gmail.com>
2017-09-21 17:19:17 -0300

    React props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+
+//Con estado
 class App extends React.Component {
   render(){
-    return <h1>Hello World</h1>
+    let txt = this.props.txt
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>{txt}</b>
+      </div>
+    )
   }
 }
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: "default txt" //Valor por defecto si no recibimos txt
+}
+
+// Sin estado
+// const App = () => <h1> Sin estado </h1>
 
 export default App
