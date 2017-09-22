a3b16291390114a3bb4ad445d372c19c859acfed
Alejandro Barreiro <alebarreiro91@gmail.com>
2017-09-22 11:51:50 -0300

    React refs

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      txt: 'this is the state txt'
+      txt: 'this is the state txt',
+      a: '',
+      b: ''
     }
   }
 
   update (e) {
-    this.setState({txt: e.target.value})
+    this.setState({
+      txt: e.target.value,
+      a: this.a.refs.input.value,
+      b: this.refs.b.value
+    })
   }
 
   render(){
@@ -21,6 +27,18 @@ class App extends React.Component {
         <h1>{this.state.txt}</h1>
         <Widget update={this.update.bind(this)}/>
         <Widget update={this.update.bind(this)}/>
+
+        <Input
+          ref={ component => this.a = component}
+          update={this.update.bind(this)}
+        /> {this.state.a}
+        <hr />
+        <input
+          ref="b"
+          type="text"
+          onChange={this.update.bind(this)}
+          /> {this.state.b}
+
       </div>
     )
   }
@@ -33,6 +51,12 @@ App.propTypes = {
 
 App.defaultProps = {
   txt: "default txt" //Valor por defecto si no recibimos txt
+}
+
+class Input extends React.Component {
+  render(){
+    return <div><input ref="input" type="text" onChange={this.props.update}/></div>
+  }
 }
 
 // Sin estado
